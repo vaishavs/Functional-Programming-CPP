1c9899ef1e82099d5eef568436ee96063c1c9071
vaishavs <avsvaishnavi@gmail.com>
2025-12-23 11:02:15 +0530

    Create ex_std_function.cpp

---
+++ 02_Designing_HO_Funcs/ex_std_function.cpp
@@ -0,0 +1,54 @@
+/* Taken from cppreference.com */
+#include <functional>
+#include <iostream>
+ 
+struct Foo
+{
+    Foo(int num) : num_(num) {}
+    void print_add(int i) const { std::cout << num_ + i << '\n'; }
+    int num_;
+};
+ 
+void print_num(int i)
+{
+    std::cout << "num = " << i << '\n';
+}
+ 
+struct PrintNum
+{
+    void operator()(int i) const
+    {
+        std::cout << i << '\n';
+    }
+};
+ 
+int main()
+{
+    // store a free function
+    std::function<void(int)> f_display = print_num;
+    f_display(-9);
+ 
+    // store a lambda
+    std::function<void()> f_display_42 = []() { print_num(42); };
+    f_display_42();
+ 
+    // store the result of a call to std::bind
+    std::function<void()> f_display_31337 = std::bind(print_num, 31337);
+    f_display_31337();
+ 
+    // store a call to a member function
+    std::function<void(const Foo&, int)> f_add_display = &Foo::print_add;
+    const Foo foo(314159);
+    f_add_display(foo, 1);
+    f_add_display(314159, 1);
+ 
+    // store a call to a data member accessor
+    std::function<int(Foo const&)> f_num = &Foo::num_;
+    std::cout << "num_: " << f_num(foo) << '\n';
+ 
+    // store a call to a functor
+    std::function<void(int)> f_display_obj = PrintNum();
+    f_display_obj(18);
+
+    return 0;
+ }
