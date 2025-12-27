f485f1a84d9ed56b1a638dcaed94849326d592b6
vaishavs <avsvaishnavi@gmail.com>
2025-12-27 10:48:05 +0530

    Update ex_fref.cpp

--- 02_Designing_HO_Funcs/ex_fref.cpp
+++ 02_Designing_HO_Funcs/ex_fref.cpp
@@ -6,4 +6,5 @@ void greet() { std::cout << "Hello!"; }
 int main() {
     void (&ref)() = greet; // ref is now an alias for greet
     ref();                 // Calls greet()
+    // Raw function references can be used in a manner similar to function pointers
 }
